1f58b8b52d7eef44c70c233faa8514eb8a0ccf65
Mohammad Shafi <umershafi02@gmail.com>
2024-08-29 23:26:13 -0700

    installed google cloud storage

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -2,10 +2,15 @@ import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "us-raw-videos02";
 
 export const createUser = functions.auth.user().onCreate((user) => {
     const userInfo = { // can I use other data?
@@ -18,3 +23,5 @@ export const createUser = functions.auth.user().onCreate((user) => {
     logger.info(`User Created: ${JSON.stringify(userInfo)}`);
     return;
 });
+
+export const generateUploadUrl = onCall({maxInstances:})
